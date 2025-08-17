8525f02ddee14984324a6b7924597df04a66463a
Anirudha Bele <beleanirudha8@gmail.com>
2025-08-17 12:38:47 +0530

    feat(03-learn-ejs/index.js): demonstrate ejs template engine

--- 03. learn-ejs/index.js
+++ 03. learn-ejs/index.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-const port = 8080;
+//set the view engine as ejs
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+//set the directory for the views
+app.set("views", path.join(__dirname, "views"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const products = [
+	{
+		id: 1,
+		title: "Product 1",
+	},
+	{
+		id: 2,
+		title: "Product 2",
+	},
+	{
+		id: 3,
+		title: "Product 3",
+	},
+];
+
+app.get("/", (req, res) => {
+	res.render("home", { title: "Home", products: products });
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", { title: "About page" });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+	console.log("server is running");
+});
