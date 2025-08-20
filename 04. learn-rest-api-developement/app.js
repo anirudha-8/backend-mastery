a401b960b86df82996b2aef3fc5fbf5388c327c4
Anirudha Bele <beleanirudha8@gmail.com>
2025-08-20 13:57:30 +0530

    feat(04-learn-rest-api-development/app.js): create dummy books data using array of objects

--- 04. learn-rest-api-developement/app.js
+++ 04. learn-rest-api-developement/app.js
@@ -5,6 +5,17 @@ const app = express();
 //Middleware
 app.use(express.json());
 
+let books = [
+	{
+		id: "1",
+		title: "Book 1",
+	},
+	{
+		id: "2",
+		title: "Book 2",
+	},
+];
+
 const port = 8080;
 
 app.get("/", (req, res) => res.send("Hello World!"));
