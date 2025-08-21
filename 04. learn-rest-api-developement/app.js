2993e1a10ab69e9ca0160b7c23d6b8b3e1047400
Anirudha Bele <beleanirudha8@gmail.com>
2025-08-21 13:59:15 +0530

    feat(04-learn-rest-api-development/app.js): create add new book route

--- 04. learn-rest-api-developement/app.js
+++ 04. learn-rest-api-developement/app.js
@@ -40,6 +40,20 @@ app.get("/get/:id", (req, res) => {
 	}
 });
 
+//add a new book
+app.post("/add", (req, res) => {
+	const newBook = {
+		id: Math.floor(Math.random() * 1000).toString(),
+		title: `Book ${Math.floor(Math.random() * 1000)}`,
+	};
+
+	books.push(newBook);
+	res.status(200).json({
+		data: newBook,
+		message: "New book is added successfully",
+	});
+});
+
 const port = 8080;
 
 app.get("/", (req, res) => res.send("Hello World!"));
