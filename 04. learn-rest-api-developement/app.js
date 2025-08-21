7b3ae697e77fb1ebe2211bc91af79482d2af954c
Anirudha Bele <beleanirudha8@gmail.com>
2025-08-21 14:00:24 +0530

    refactor(04-learn-rest-api-development/app.js): refactor code

--- 04. learn-rest-api-developement/app.js
+++ 04. learn-rest-api-developement/app.js
@@ -16,19 +16,19 @@ let books = [
 	},
 ];
 
-//intro route
+// intro route
 app.get("/", (req, res) => {
 	res.json({
 		message: "Welcome to our bookstore api",
 	});
 });
 
-//get all books
+// get all books
 app.get("/get", (req, res) => {
 	res.json(books);
 });
 
-//get a single book
+// get a single book
 app.get("/get/:id", (req, res) => {
 	const book = books.find((item) => item.id === req.params.id);
 	if (book) {
@@ -40,7 +40,7 @@ app.get("/get/:id", (req, res) => {
 	}
 });
 
-//add a new book
+// add a new book
 app.post("/add", (req, res) => {
 	const newBook = {
 		id: Math.floor(Math.random() * 1000).toString(),
