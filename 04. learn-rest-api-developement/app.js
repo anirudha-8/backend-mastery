99c2824c5b98a33ffa24142f19acbe6ec6d7d87b
Anirudha Bele <beleanirudha8@gmail.com>
2025-08-22 14:04:53 +0530

    feat(04-learn-rest-api-development/app.js): create update book route

--- 04. learn-rest-api-developement/app.js
+++ 04. learn-rest-api-developement/app.js
@@ -54,6 +54,25 @@ app.post("/add", (req, res) => {
 	});
 });
 
+//update a book
+app.put("/update/:id", (req, res) => {
+	const findCurrentBook = books.find(
+		(bookItem) => bookItem.id === req.params.id
+	);
+	if (findCurrentBook) {
+		findCurrentBook.title = req.body.title || findCurrentBook.title;
+
+		res.status(200).json({
+			message: `Book with ID ${req.params.id} updated successfully`,
+			data: findCurrentBook,
+		});
+	} else {
+		res.status(404).json({
+			message: "Book not found",
+		});
+	}
+});
+
 const port = 8080;
 
 app.get("/", (req, res) => res.send("Hello World!"));
