5ac8098d08f10d3f7755dd8cf701969b5a15d581
Anirudha Bele <beleanirudha8@gmail.com>
2025-09-08 14:27:39 +0530

    feat(09-book-store-api/server.js): update server.js

--- 09. book-store-api/server.js
+++ 09. book-store-api/server.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const port = 3000;
+const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// HOME route
+app.get("/", (req, res) => res.send("Home Route!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => {
+	console.log(`Server is running at: http://localhost:${PORT}!`);
+});
