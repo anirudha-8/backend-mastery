0f584a035c7abaf2a0c882452f5d107a213a7280
Anirudha Bele <beleanirudha8@gmail.com>
2025-09-09 14:33:27 +0530

    feat(09-book-store-api/server.js): import and use book api routes in server.js

--- 09. book-store-api/server.js
+++ 09. book-store-api/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const bookRoutes = require("./routes/bookRoutes");
 
 const app = express();
 
@@ -14,6 +15,9 @@ app.use(express.json());
 
 // HOME route
 app.get("/", (req, res) => res.send("Home Route!"));
+
+// book api routes
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server is running at: http://localhost:${PORT}!`);
