5b98f31753c1127aac7a2604860ccf25e6105cfb
Anirudha Bele <beleanirudha8@gmail.com>
2025-09-11 14:56:54 +0530

    feat(09-book-store-api/models/Book.js): create mongodb collection schema for book

--- 09. book-store-api/models/Book.js
+++ 09. book-store-api/models/Book.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
 
 const BookSchema = new mongoose.Schema({
-	title: {},
-	author: {},
-	year: {},
-	createdAt: {},
+	title: {
+		type: String,
+		required: [true, "Book title is required!"],
+		trim: true,
+		maxLength: [100, "Book title cannot be more that 100 characters!"],
+	},
+	author: {
+		type: String,
+		required: [true, "Author name is required!"],
+		trim: true,
+	},
+	year: {
+		type: Number,
+		required: [true, "Publication year is required!"],
+		min: [1000, "Year must be at least 100!"],
+		max: [new Date().getFullYear(), "Year cannot be in the future!"],
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
 });
 
 module.exports = mongoose.model("Book", BookSchema);
