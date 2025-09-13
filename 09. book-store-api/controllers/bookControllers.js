63a40e7829afec949462fa5fd1346df12a075a0d
Anirudha Bele <beleanirudha8@gmail.com>
2025-09-13 15:13:34 +0530

    feat(09-book-store-api/controllers/bookControllers.js): define all controllers logic baseline

---
+++ 09. book-store-api/controllers/bookControllers.js
@@ -0,0 +1,7 @@
+const Book = require("../models/Book");
+
+const getAllBooks = async (req, res) => {};
+const getSingleBookById = async (req, res) => {};
+const addNewBook = async (req, res) => {};
+const deleteBook = async (req, res) => {};
+const updateBook = async (req, res) => {};
