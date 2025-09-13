8f2acdbe8f5be41341d431da455e265fc198004f
Anirudha Bele <beleanirudha8@gmail.com>
2025-09-13 15:19:22 +0530

    feat(09-book-store-api/controllers/bookControllers.js): add detailed description of all controllers routes

--- 09. book-store-api/controllers/bookControllers.js
+++ 09. book-store-api/controllers/bookControllers.js
@@ -1,7 +1,26 @@
 const Book = require("../models/Book");
 
+// @desc 	Get all books
+// @route 	GET /api/books/get
+// @access 	Public
 const getAllBooks = async (req, res) => {};
+
+// @desc 	Get single book by ID
+// @route 	GET /api/books/get/:id
+// @access 	Public
 const getSingleBookById = async (req, res) => {};
+
+// @desc 	Create a new book
+// @route 	POST /api/books/add
+// @access 	Public
 const addNewBook = async (req, res) => {};
+
+// @desc 	Delete a book
+// @route 	DELETE /api/books/delete/:id
+// @access 	Public
 const deleteBook = async (req, res) => {};
+
+// @desc 	Update a book
+// @route 	PUT /api/books/update/:id
+// @access 	Public
 const updateBook = async (req, res) => {};
