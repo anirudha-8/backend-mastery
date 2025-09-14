51557ac06808c2b781fbfb6a421fe4b0c3fc58f1
Anirudha Bele <beleanirudha8@gmail.com>
2025-09-14 15:23:24 +0530

    feat(09-book-store-api/routes/bookRoutes.js): import all controllers definitions and use it in all the book api routes

--- 09. book-store-api/routes/bookRoutes.js
+++ 09. book-store-api/routes/bookRoutes.js
@@ -1,20 +1,27 @@
 import express from "express";
+import {
+	addNewBook,
+	deleteBook,
+	getAllBooks,
+	getSingleBookById,
+	updateBook,
+} from "../controllers/bookControllers";
 
 const router = express.Router();
 
 // @desc - get all books
-router.get("/get");
+router.get("/get", getAllBooks);
 
 // @desc - get single book by ID
-router.get("/get/:id");
+router.get("/get/:id", getSingleBookById);
 
 // @desc - create new book
-router.post("/add");
+router.post("/add", addNewBook);
 
 // @desc - delete a book
-router.delete("/delete/:id");
+router.delete("/delete/:id", deleteBook);
 
 // @desc - update a book
-router.put("/update/:id");
+router.put("/update/:id", updateBook);
 
 export default router;
