2781a6b73e602dbd440c48a675fb39d8f7a791fd
Anirudha Bele <beleanirudha8@gmail.com>
2025-09-18 16:09:20 +0530

    feat(09-book-store-api/config/db.js): add MongoDB URI connection string

--- 09. book-store-api/config/db.js
+++ 09. book-store-api/config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
 const connectDB = async () => {
 	try {
-		await mongoose.connect();
+		await mongoose.connect(MONGODB_URI);
 		console.log("MongoDB is connected successfully!");
 	} catch (error) {
 		console.error("MongoDB connection failed!");
