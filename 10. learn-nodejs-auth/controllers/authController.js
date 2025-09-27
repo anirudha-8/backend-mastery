264e12326e6e192fecfeaaf51d4755fd8d98ba60
Anirudha Bele <beleanirudha8@gmail.com>
2025-09-28 03:50:30 +0530

    feat(10-learn-nodejs-auth/controllers/authController.js): create register controller basic structure

---
+++ 10. learn-nodejs-auth/controllers/authController.js
@@ -0,0 +1,11 @@
+// register user
+const registerUser = async (req, res) => {
+	try {
+	} catch (error) {
+		console.error(`Error in register user: ${error}`);
+		res.status(500).json({
+			success: false,
+			message: "Internal server error, please try again later!",
+		});
+	}
+};
