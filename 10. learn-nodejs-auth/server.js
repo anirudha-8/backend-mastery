ef0bd0164c8a861e22a2f4ce678a3996cf12ef6d
Anirudha Bele <beleanirudha8@gmail.com>
2025-10-01 04:01:20 +0530

    feat(10-learn-nodejs-auth/server.js): use auth routes in server file

--- 10. learn-nodejs-auth/server.js
+++ 10. learn-nodejs-auth/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 // connect to MongoDB
 connectDB();
@@ -9,8 +10,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// routes
+// home route
 app.get("/", (req, res) => res.send("Hello World!"));
+
+// API related routes
+app.use("/api/auth", authRoutes);
 
 // listening to the server
 app.listen(PORT, () => {
