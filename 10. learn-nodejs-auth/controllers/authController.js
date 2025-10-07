1b9621f152631e784bd8b9382177d980c58d946b
Anirudha Bele <beleanirudha8@gmail.com>
2025-10-07 21:08:18 +0530

    feat(10-learn-nodejs-auth/controllers/authController.js): extract user information from request body

--- 10. learn-nodejs-auth/controllers/authController.js
+++ 10. learn-nodejs-auth/controllers/authController.js
@@ -3,6 +3,8 @@
 // @access 	Public
 const registerUser = async (req, res) => {
 	try {
+		// extract user information from request body
+		const { username, email, password, role } = req.body;
 	} catch (error) {
 		console.error(`Error in register user: ${error}`);
 		res.status(500).json({
