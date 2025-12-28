7fb45e4033c5fb4f57aecfad98dc87b2195f6ea3
Anirudha Bele <beleanirudha8@gmail.com>
2025-12-28 12:14:13 +0530

    feat(10-learn-nodejs-auth/server.js): import db connection logic in server.js

--- 10. learn-nodejs-auth/server.js
+++ 10. learn-nodejs-auth/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
-
 const express = require("express");
+const connectToDB = require("./config/db");
 
 const PORT = process.env.PORT || 8080;
+
+// connect to database
+connectToDB();
+
 const app = express();
 
 app.listen(PORT, () => {
